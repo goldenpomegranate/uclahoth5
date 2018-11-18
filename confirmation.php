4672cd8a26adddc85e71773e16fe5914839886c7
lilicchi <39143919+lilicchi@users.noreply.github.com>
2018-11-17 19:31:16 -0800

    Create confirmation.php

---
+++ confirmation.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<p>Please confirm that the information you inputted is correct: </p>
+<br><br>
+
+
+
+Recipe Title: <?php echo $_POST["title"]; ?><br>
+Time to Prep: <?php echo $_POST["preptime"]; ?><br>
+Time to Cook: <?php echo $_POST["cooktime"]; ?><br>
+Ready in: <?php echo $_POST["readyIn"]; ?><br>
+Ingredients: <?php echo $_POST["ingredientList"]; ?><br>
+Steps: <?php echo $_POST["stepsList"]; ?><br>
+
+</body>
+</html>
